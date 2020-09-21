96b299c07f76ca5a15c1c61b4d6b912df2f3f131
Fabian Gorsler <gorsler@mailbox.org>
2020-09-21 10:10:21 +0200

    Minor improvements, listens for all HTTP methods

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,11 @@ FROM node:12
 EXPOSE 3000
 
 WORKDIR /usr/src/app
+RUN chown -R node /usr/src/app
+
+USER node
+
 COPY . .
 RUN npm install
 
-CMD ["node", "app.js"]
+CMD ["bin/www"]
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,17 @@ var moment = require('moment')
 var os = require('os');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/*', function(req, res, next) {
-  res.render('index',
-      {
-        title: 'Web Echo',
-        hostname: os.hostname(),
-        date: moment().format(),
-        method: req.method,
-        url: req.url,
-        headers: req.headers,
-        body: req.body.content
-      });
+router.all('/*', function(req, res, next) {
+    res.render('index',
+        {
+            title: 'webecho',
+            hostname: os.hostname(),
+            date: moment().format(),
+            method: req.method,
+            url: req.url,
+            headers: req.headers,
+            body: req.body.content
+        });
 });
 
 module.exports = router;
